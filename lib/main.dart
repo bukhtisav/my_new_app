ed5d9a732526aafc7d320ea337c348afe307b2b4
magomedtagir <bukhtisav@mail.ru>
2025-06-03 19:02:02 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+    return MaterialApp(
+      theme: ThemeData(primaryColor: Colors.blueAccent),
+      home: RandomWords()
+    );
+  }
+}
+
+class RandomWords extends StatefulWidget{
+  @override
+  RandomWordsState createState () => RandomWordsState() 
+}
