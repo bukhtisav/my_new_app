6dd957e4d2630d91e9345e568c464f02b74e58f8
magomedtagir <bukhtisav@mail.ru>
2025-06-15 11:36:54 +0300

    Добавил экран избранного и логику удаления

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
+import 'random_words.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,15 +10,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
     return MaterialApp(
       theme: ThemeData(primaryColor: Colors.blueAccent),
-      home: RandomWords()
+      home: RandomWords(),
     );
   }
-}
-
-class RandomWords extends StatefulWidget{
-  @override
-  RandomWordsState createState () => RandomWordsState() 
 }
